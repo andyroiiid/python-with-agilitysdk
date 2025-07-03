bbcd1a2f4dd5c85eaf047af1f2b4450f482bd5ab
Andrew Huang <andyroiiid@live.com>
2025-07-03 00:00:31 -0500

    Add DLL exports to enable Agility SDK

--- Programs/python.c
+++ Programs/python.c
@@ -3,6 +3,11 @@
 #include "Python.h"
 
 #ifdef MS_WINDOWS
+// Don't really care about Python coding style here
+// This fork is a hack to add Agility SDK support
+__declspec(dllexport) extern const uint32_t D3D12SDKVersion = 616;
+__declspec(dllexport) extern const char* D3D12SDKPath = u8".\\D3D12\\";
+
 int
 wmain(int argc, wchar_t **argv)
 {
